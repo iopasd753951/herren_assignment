fdb50c633a1eac983085e02f49706f846bcab7ed
iopasd753951 <iopasd753951@gmail.com>
2020-10-23 19:10:20 +0900

    [수정] accounts_api 앱 구조 변경

--- accounts_api/views.py
+++ accounts_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from accounts_api import models, serializers
+
+
+class UserAccountViewSet(viewsets.ModelViewSet):
+    """ 유저 생성, 업데이트 핸들러 """
+
+    serializer_class = serializers.UserAccountSerializer
+    queryset = models.UserAccount.objects.all()
+
+
+class LoginViewSet(viewsets.ViewSet):
+    """ 유저 로그인 이메일과 패스워드 """
+
+    # serializers_class = AuthTokenSerializer
