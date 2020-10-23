0a1a0cdb236c661c595397b5872ec36ebc9cd8c8
iopasd753951 <iopasd753951@gmail.com>
2020-10-23 17:14:00 +0900

    [추가] 유저계정 Serializers 추가

--- accounts_api/api/serializers.py
+++ accounts_api/api/serializers.py
@@ -3,5 +3,22 @@ from rest_framework import serializers
 from accounts_api import models
 
 
-class UserRegisterSerializer(serializers.ModelSerializer):
-    model = models.UserRegister
+class UserAccountSerializer(serializers.ModelSerializer):
+    """ 유저 계정 시리얼라이저 """
+
+    model = models.UserAccount
+    fields = ('email, name, password')
+    extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        """ (POST) 유저 생성 """
+
+        user = models.UserAccount(
+            email=validated_data['email'],
+            name=validated_data['name'],
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
