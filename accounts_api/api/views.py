f7a5a76c3b581929182f6ba2d46da99852f090c8
iopasd753951 <iopasd753951@gmail.com>
2020-10-23 17:15:14 +0900

    [추가] 유저 생성, 업데이트 ViewSet

--- accounts_api/api/views.py
+++ accounts_api/api/views.py
@@ -1,7 +1,16 @@
 from rest_framework import viewsets
 
+from . import serializers
+from accounts_api import models
 
-class UserRegisterViewSet(viewsets.ModelViewSet):
-    """ 유저 생성 ViewSet """
 
+class UserAccountViewSet(viewsets.ModelViewSet):
+    """ 유저 생성, 업데이트 핸들러 """
+
+    serializer_class = serializers.UserAccountSerializer
+    queryset = models.UserAccount.objects.all()
+
+
+class LoginViewSet(viewsets.ViewSet):
+    """ 유저 로그인 이메일과 패스워드 """
     pass
