8c4a53c5c351d067efd1efcfe9585f289c72f048
iopasd753951 <iopasd753951@gmail.com>
2020-10-24 09:52:19 +0900

    [추가] 로그인 토큰추가

--- accounts_api/views.py
+++ accounts_api/views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
 
-from accounts_api import models, serializers
+from . import models, serializers, permission
 
 
 class UserAccountViewSet(viewsets.ModelViewSet):
@@ -8,9 +11,16 @@ class UserAccountViewSet(viewsets.ModelViewSet):
 
     serializer_class = serializers.UserAccountSerializer
     queryset = models.UserAccount.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permission.UpdateOwnAccount,)
 
 
 class LoginViewSet(viewsets.ViewSet):
     """ 유저 로그인 이메일과 패스워드 """
 
-    # serializers_class = AuthTokenSerializer
+    serializers_class = AuthTokenSerializer
+
+    def create(self, request):
+        """ 토큰검사 및 토큰발급 """
+
+        return ObtainAuthToken.post(request)
