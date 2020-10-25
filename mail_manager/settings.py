09fac0d436565d985ddc19a786705924031f9ff2
iopasd753951 <iopasd753951@gmail.com>
2020-10-25 21:31:20 +0900

    [추가] ALLOWED_HOSTS 추가

--- mail_manager/settings.py
+++ mail_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3i-oq!u8z(p8j3+^-7$)@$u6j!y9o1=nn1_(x6$34=mkzeoa@q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
