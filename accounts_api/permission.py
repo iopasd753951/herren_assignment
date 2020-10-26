67d61899e6fa67cdcbee41c115bca9cc1048c92f
iopasd753951 <iopasd753951@gmail.com>
2020-10-26 19:44:13 +0900

    [추가] 메일링리스트 permission

--- accounts_api/permission.py
+++ accounts_api/permission.py
@@ -6,9 +6,21 @@ class UpdateOwnAccount(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         """ Safe Method를 제외한 Method가 올 시 """
-        """ 쓰기권한을 소유자에게 부여 """
+        """ 계정정보수저 쓰기권한을 소유자에게 부여 """
 
         if request.method in permissions.SAFE_METHODS:
             return True
 
         return obj.email == request.user.email
+
+
+class UpdateOwnMailList(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        """ 메일링리스트 쓰기 권한을 소유자에게 부여"""
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user == request.user.user
+
