c41f642bfde5bab8237391e1f6662689efe79eed
iopasd753951 <iopasd753951@gmail.com>
2020-10-26 19:46:00 +0900

    [추가] 메일링리스트 viewset

--- accounts_api/views.py
+++ accounts_api/views.py
@@ -1,8 +1,9 @@
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.settings import api_settings
+from rest_framework.decorators import action
 
 from . import models, serializers, permission
 
@@ -12,11 +13,24 @@ class UserAccountViewSet(viewsets.ModelViewSet):
 
     serializer_class = serializers.UserAccountSerializer
     queryset = models.UserAccount.objects.all()
-    # authentication_classes = (TokenAuthentication,)
+    authentication_classes = (TokenAuthentication,)
     permission_classes = (permission.UpdateOwnAccount,)
+
+    @action(detail=True, methods=['post'])
+    def leave(self, request):
+        pass
 
 
 class UserLoginApiView(ObtainAuthToken):
     """ 유저 로그인 이메일과 패스워드 """
 
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class UserMailListViewSet(viewsets.ModelViewSet):
+    """ 유저 메일 리스트 핸들러 """
+
+    queryset = models.UserMailList.objects.all()
+    serializer_class = serializers.UserMailListSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permission.UpdateOwnAccount,)
